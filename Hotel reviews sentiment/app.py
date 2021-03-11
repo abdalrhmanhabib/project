1a234513f491283c72dd171839132f34affdb101
abdalrhmanhabib <77023719+abdalrhmanhabib@users.noreply.github.com>
2021-03-12 01:05:37 +0200

    Add files via upload

---
+++ Hotel reviews sentiment/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import pickle
+import csv
+
+model = pickle.load(open('reviews.pkl', 'rb'))
+
+app = Flask(__name__,template_folder='templates')
+
+
+
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    data1 = request.form['a']
+
+    pred = model.predict([data1])
+    myfile=open('review record.csv','a',encoding='utf-8')
+    wr=csv.writer(myfile)
+    wr.writerow([data1,pred])
+    myfile.close()
+    
+    
+    
+    return render_template('after.html', data=pred)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
